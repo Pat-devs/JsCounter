46aea6ded906b902966a69d199f637e791b41c67
Pat-devs <patryk.kodehode@gmail.com>
2025-11-10 09:50:50 +0100

    queryselector & queryselectorall

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("test")
+/* 
+<h2 id="counter-value">0</h2>
+<button id="button-increment">+</button>
+<button id="button-decrement">-</button>
+<button id="button-reset">Reset counter</button>
+*/
+
+// selecting elements with javascript
+
+// #1 using the querySelector method
+
+let buttonElement = document.querySelector("button")
+
+console.log(buttonElement)
+
+
+// #1b to select all elements that match a query:
+
+let allButtons = document.querySelectorAll("button")
+
+console.log(allButtons)
