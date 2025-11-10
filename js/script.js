88aee46122c912ff38211e85c8ab9bb2d992dfd4
Pat-devs <patryk.kodehode@gmail.com>
2025-11-10 10:28:45 +0100

    function example

--- js/script.js
+++ js/script.js
@@ -1,16 +1,51 @@
-/* 
-<h2 id="counter-value">0</h2>
-<button id="button-increment">+</button>
-<button id="button-decrement">-</button>
-<button id="button-reset">Reset counter</button>
-*/
+// declare a numerical variable for the counter
+let counter = 0
+
+
 
 // select all the buttons, and store them in each variable
 let buttonIncrement = document.querySelector("#button-increment")
 let buttonDecrement = document.querySelector("#button-decrement")
 let buttonResetCounter = document.querySelector("#button-reset")
 
-console.log(buttonIncrement)
-console.log(buttonDecrement)
-console.log(buttonResetCounter)
+// Select the counter-value element
+let counterValueElement = document.querySelector("#counter-value")
 
+// print out the value of the counter-value element:
+//console.log(counterValueElement.textContent)
+
+// increment  the counter-value element value
+counterValueElement.textContent = counterValueElement.textContent + 1
+
+// print out the value of the counter-value element:
+//console.log(counterValueElement.textContent)
+
+
+
+
+
+
+// declare a function called greet
+function greet() {
+    // inside function body {...}
+    // we can have some code
+
+    console.log("Hi")
+    console.log("How are you?")
+}
+
+// to run a function, write its name, followed by ()
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()
+
+// increment counter variable
+// counter = counter + 1
+
+// print counter variable
+// console.log(counter)
